789d7b13286bc6e35721cd3babf679fbd9f2f650
areumz <ksah1016@naver.com>
2026-08-25 21:39:29 +0900

    feat: project initial setup

---
+++ src/main/java/com/areumz/devqueue/DevqueueApplication.java
@@ -0,0 +1,13 @@
+package com.areumz.devqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/areumz/devqueue/DevqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areumz.devqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
